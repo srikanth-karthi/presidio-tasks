3e6b7b8669e549febfe7cf9a5bcf8ca91b823d7e
srikanth-karthi <srikanthkarthi2003@gmail.com>
2024-04-12 12:16:30 +0530

    update

---
+++ 12.04.2024/Opps/Opps/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opps
+{
+    internal class Employee
+    {
+
+       public string name {  get; set; }
+       public void work()
+        {
+            Console.WriteLine("it works");
+        }
+
+    }
+}
---
+++ 12.04.2024/Opps/Opps/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Opps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+     
+            Employee employee = new Employee();
+            employee.name = "Srikanth";
+            Console.WriteLine(employee.name);
+            employee.work();
+        }
+    }
+}
