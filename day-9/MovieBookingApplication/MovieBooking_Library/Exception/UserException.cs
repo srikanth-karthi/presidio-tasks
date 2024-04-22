011fca947edcb46b4247130b8cc89cca9052e1ed
srikanth-karthi <srikanthkarthi2003@gmail.com>
2024-04-22 14:42:50 +0530

    created model classes

---
+++ day-9/MovieBookingApplication/MovieBooking_Library/Exception/UserException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class UserException : Exception
+{
+    public UserException(string message)
+        : base(message)
+    {
+    }
+}
+public class UserValidationException : Exception
+{
+    public UserValidationException(string message) : base(message) { }
+}
---
+++ day-9/MovieBookingApplication/MovieBooking_Library/Service/UserService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MovieBooking_Library.Models;
+
+namespace MovieBooking_Library.Service
+{
+
+    public class UserService
+    {
+        public static User loggedInUser = null;
+        private UserRepository userRepository;
+
+        public UserService()
+        {
+            userRepository = new UserRepository();
+        }
+
+        public User CreateUser(User newUser)
+        {
+            return userRepository.Add(newUser);
+        }
+
+        public User LoginUser(string username, string password)
+        {
+            User user = userRepository.Get(username);
+            if (user.Password == password)
+            {
+                Console.WriteLine($"Welcome, {username}!");
+                loggedInUser=user;
+                return user;
+            }
+
+            throw new UserException("Invalid username or password.");
+        }
+
+        public void Userlogout()
+        {
+
+            loggedInUser = null;
+        }
+    }
+}
+
+
+
+
+
