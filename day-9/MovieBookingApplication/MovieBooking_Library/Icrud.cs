bc993c4d1cc6634afa85912dbda5450a37bde5aa
srikanth-karthi <srikanthkarthi2003@gmail.com>
2024-04-22 14:55:21 +0530

    created a movie booking Application With 3 tire architecture

---
+++ day-9/MovieBookingApplication/MovieBooking_Library/Icrud.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MovieBooking_Library
+{
+    public interface IRepository<K, T> where T : class
+    {
+        Dictionary<K, T> GetAll();
+        T Get(K key);
+        T Add(T item);
+        T Update(T item);
+        T Delete(K key);
+    }
+}
---
+++ day-9/MovieBookingApplication/MovieBooking_Library/Models/Movie.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MovieBooking_Library.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public DateTime ScreeningTime { get; set; }
+        public bool[] Seats { get; set; }
+        public double Price { get; set; }
+
+        // Parameterized constructor
+        public Movie(string title, string genre, int duration, DateTime screeningTime, double price)
+        {
+            Title = title;
+            Genre = genre;
+            Duration = duration;
+            ScreeningTime = screeningTime;
+            Seats = new bool[50]; // 50 seats in the theater
+            Price = price;
+        }
+
+        public void PrintSeats()
+        {
+            Console.WriteLine("\nSeats in the theater:");
+            for (int i = 0; i < Seats.Length; i++)
+            {
+                if (Seats[i])
+                {
+                    Console.Write($"X{i + 1} "); 
+                }
+                else
+                {
+                    Console.Write($"_{i + 1} "); 
+                }
+
+                if ((i + 1) % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+
+
+        public override string ToString()
+        {
+            return $"Title: {Title}, Genre: {Genre}, Duration: {Duration} minutes, Screening Time: {ScreeningTime}, Price: {Price}";
+        }
+    }
+}
