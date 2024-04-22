2aef7c0f384f0bdd6da176012ae5ca34d309e0fc
srikanth-karthi <srikanthkarthi2003@gmail.com>
2024-04-22 15:24:11 +0530

    create a Feedback model

---
+++ day-9/MovieBookingApplication/MovieBooking_Library/Service/FeedBackService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieBooking_Library.Service
+{
+    internal class FeedBackService
+    {
+    }
+}
