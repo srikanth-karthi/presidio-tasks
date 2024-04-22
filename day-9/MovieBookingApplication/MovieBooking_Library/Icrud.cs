6d5fe8ab61fdf600f93d2c743961a9f9c511e01f
srikanth-karthi <srikanthkarthi2003@gmail.com>
2024-04-22 14:46:14 +0530

    created model classes

--- day-9/MovieBookingApplication/MovieBooking_Library/Icrud.cs
+++ day-9/MovieBookingApplication/MovieBooking_Library/Icrud.cs
@@ -1,13 +1,1 @@
-﻿using System.Collections.Generic;
-
-namespace MovieBooking_Library
-{
-    public interface IRepository<K, T> where T : class
-    {
-        Dictionary<K, T> GetAll();
-        T Get(K key);
-        T Add(T item);
-        T Update(T item);
-        T Delete(K key);
-    }
-}
+﻿
